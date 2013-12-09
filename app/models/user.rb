138771f2571100ccecd68e886476a56a1600e92b
Michael Malchak <michaelmalchak@gmail.com>
2013-12-09 15:09:40 -0800

    associate users and groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :rounds
 	has_many :courses, :through => :rounds
+	has_and_belongs_to_many :groups
 
 	validates_presence_of :first_name, :last_name, :email, :password
 	validates_uniqueness_of :email
